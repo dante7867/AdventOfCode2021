aebb1faa8495ebc9b45f88fd2f6c5a9263f5e5c2
dante7867 <dante7867@gmail.com>
2021-12-12 20:32:20 +0100

    get rid of deepcopies, refactor

--- 12/12.py
+++ 12/12.py
@@ -2,21 +2,22 @@
 # https://adventofcode.com/2021/day/12
 
 from collections import Counter, defaultdict
-from copy import deepcopy
 
 
-def walk(c, h, visited, allowed):
+def walk(c, visited, allowed):
+    global paths
+
     if c.islower():
         visited[c] += 1
     if allowed:
         if 2 in visited.values():
             allowed = False
     for p in connected[c]:
-        if p.isupper() or (p.islower() and (visited[p] < 1 or allowed)) and p != 'start':
+        if p.isupper() or (visited[p] < 1 or allowed) and p != 'start':
             if p == 'end':
-                paths.add(h + ',' + p)
+                paths += 1
             else: 
-                walk(p, h + ',' + p,deepcopy(visited), deepcopy(allowed))
+                walk(p, visited.copy(), allowed)
 
 
 connected = defaultdict(set)
@@ -26,11 +27,11 @@ with open('i.txt','r') as f:
         connected[line[0]].add(line[1])
         connected[line[1]].add(line[0])
 
-paths = set()
+paths = 0
+walk('start', Counter(), False)
+print('p1 =', paths)
 
-walk('start', 'start', Counter(), False)
-print(f'p1 = {len(paths)}')
-
-walk('start', 'start', Counter(), True)
-print(f'p2 = {len(paths)}')
+paths = 0
+walk('start', Counter(), True)
+print('p2 =', paths)
 
